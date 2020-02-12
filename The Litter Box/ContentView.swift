84b7267d984f3f6feb6d4a9c83d7f684d436a21c
Chris <cwilli34@samford.edu>
2020-02-12 11:28:47 -0600

    Added basic map

--- The Litter Box/ContentView.swift
+++ The Litter Box/ContentView.swift
@@ -7,15 +7,44 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            MapView()
+            .navigationBarTitle(Text("Map View"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    // 1.
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    // 2.
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        // 3.
+        let location = CLLocationCoordinate2D(latitude: 51.50007773,
+            longitude: -0.1246402)
+        // 4.
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        uiView.setRegion(region, animated: true)
+        
+        // 5.
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        uiView.addAnnotation(annotation)
     }
 }
