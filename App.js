dc216b1865b4d35c4a65eacb41a67b504590b670
Chris <cwilli34@samford.edu>
2020-02-25 10:57:05 -0600

    TC-17: Add google maps

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { ResizeObserver } from 'resize-observer-polyfill'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export class App extends Component {
+  render() {
+    return (
+      <ErrorBoundary>
+        <div className = "App">
+          <Map
+            google={this.props.google}
+            zoom={8}
+            style={mapStyles}
+            initialCenter={{ lat: 47.444, lng: -122.176 }}
+          />
+        </div>
+      </ErrorBoundary>
+    );
+  }
+}
+
+export default GoogleApiWrapper({apiKey: ('AIzaSyCjt-279EWJO9s_rMWB4rMq8EQMzLg8xEU')})(App);
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
