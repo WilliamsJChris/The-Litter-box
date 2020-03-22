0f7f49dc8eb4920fe5d733a42da6f2e6407fa7c6
Chris <cwilli34@samford.edu>
2020-03-22 17:54:37 -0500

    TC-1: MapKit Integration Roll Back
    Roll back to MapKit and Swift with a few more pins dropped

--- The Litter Box/ContentView.swift
+++ The Litter Box/ContentView.swift
@@ -25,26 +25,47 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct MapView: UIViewRepresentable {
-    // 1.
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
         MKMapView(frame: .zero)
     }
     
-    // 2.
     func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
-        // 3.
-        let location = CLLocationCoordinate2D(latitude: 51.50007773,
-            longitude: -0.1246402)
-        // 4.
+        // Start Frame Location
+        let startLocation = CLLocationCoordinate2D(latitude: 33.44492917135115,
+            longitude: -86.81618575369598)
+        
+        // Start Location Annotation
+        let startAnnotation = MKPointAnnotation()
+        startAnnotation.coordinate = startLocation
+        startAnnotation.title = "Hickory Knoll"
+        startAnnotation.subtitle = "Start Location"
+        uiView.addAnnotation(startAnnotation)
+        
+        // Frame Location
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
+        let region = MKCoordinateRegion(center: startLocation, span: span)
         uiView.setRegion(region, animated: true)
         
-        // 5.
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Big Ben"
-        annotation.subtitle = "London"
-        uiView.addAnnotation(annotation)
+        // Pin Location 1
+        let location1 = CLLocationCoordinate2D(latitude: 33.4319484,
+            longitude: -86.8155857)
+        
+        // Location 1 Annotation
+        let annotation1 = MKPointAnnotation()
+        annotation1.coordinate = location1
+        annotation1.title = "Woodmere Creek"
+        annotation1.subtitle = "779 Woodmere Creek Drive"
+        uiView.addAnnotation(annotation1)
+        
+        // Pin Location 2
+        let location2 = CLLocationCoordinate2D(latitude: 33.4648253,
+            longitude: -86.7916904)
+        
+        // Location 2 Annotation
+        let annotation2 = MKPointAnnotation()
+        annotation2.coordinate = location2
+        annotation2.title = "Samford University"
+        annotation2.subtitle = "800 Lakeshore Drive"
+        uiView.addAnnotation(annotation2)
     }
 }
